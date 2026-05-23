ac8e4d8f61a81209de02e9e412742acfaee9ef9c
Nguyen Cao Thang <thangnc0401@gmail.com>
2026-05-23 19:35:03 +0700

    disable customizer

--- src/@core/layouts/VerticalLayout.tsx
+++ src/@core/layouts/VerticalLayout.tsx
@@ -147,7 +147,7 @@ const VerticalLayout = (props: LayoutProps) => {
       </VerticalLayoutWrapper>
 
       {/* Customizer */}
-      <Customizer />
+      {/* <Customizer /> */}
       {disableCustomizer || hidden ? null : <Customizer />}
 
       {/* Scroll to top button */}
